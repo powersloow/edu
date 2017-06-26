3ec724ca3887acf8a6c6bdfc47f4ad8297c26d25
2강019 <2019@192.168.0.10>
2017-06-26 11:45:48 +0900

    c1

--- HelloJava/src/com/paran/chapter3/CastingDemo.java
+++ HelloJava/src/com/paran/chapter3/CastingDemo.java
@@ -7,9 +7,9 @@ public class CastingDemo {
 	
 	public static void main(String[] args) {
 		CastingDemo cd = new CastingDemo();
-		cd.num1 = 10000000000;
-		cd.num2 = 10000000000;
-		cd.num3 = 12.45;
+		cd.num1 = 1000000000;
+		cd.num2 = 10000000000l;
+		cd.num3 = 12.45f;
 	}
 
 }
